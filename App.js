99359b5dbcef4deac760b1c88f8a3695c7fc9344
6210110371 <6210110371@psu.ac.th>
2021-07-12 13:38:32 +0700

    CHALLENGE

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Weather from './components/Weather';
-import constants from 'expo-constants';
-
-
+import Constants from 'expo-constants'
 export default function App() {
- return (
-  <View style={styles.container}>
-    <Weather zipCode="90110"/>
-    <StatusBar style="auto" />
-  </View>
- 
- );
+  const doIt = () => {
+    console.log("Hello from console")
+  }
+  return (
+    <View style={styles.container}>
+      <Weather zipCode="90110"/>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
- container: {
-  paddingTop: constants.statusBarHeight
- },
+  container: {
+    paddingTop: Constants.statusBarHeigh
+  },
 });
